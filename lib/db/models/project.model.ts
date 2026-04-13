346ea6c3c573c4bdf744d9b25b766ce870c7d168
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2026-04-13 14:54:11 +0100

    Add ProjectInput type to project model

--- lib/db/models/project.model.ts
+++ lib/db/models/project.model.ts
@@ -3,3 +3,16 @@ import { Document, Model, model, models, Schema, Types } from 'mongoose';
 export type ProjectBody = {
   code: string;
 };
+
+export type ProjectInput = {
+  slug: string;
+  title: string;
+  description: string;
+  date?: Date;
+  location?: string;
+  picture?: string;
+  url?: string;
+  repository?: string;
+  published: boolean;
+  body: ProjectBody;
+};

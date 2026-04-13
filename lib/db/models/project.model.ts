0114c3a7f4c8e300f1ef7d37b008b0a1b9d6c0b9
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2026-04-13 16:01:42 +0100

    Add project schema definition to project model

--- lib/db/models/project.model.ts
+++ lib/db/models/project.model.ts
@@ -1,4 +1,5 @@
 import { Document, Model, model, models, Schema, Types } from 'mongoose';
+import { required } from 'zod/v4/core/util.cjs';
 
 export type ProjectBody = {
   code: string;
@@ -16,3 +17,23 @@ export type ProjectInput = {
   published: boolean;
   body: ProjectBody;
 };
+
+const projectSchema = new Schema(
+  {
+    slug: { type: String, required: true, unique: true, index: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+
+    date: { type: Date, require: false },
+    location: { type: String, require: false },
+    picture: { type: String, require: false },
+    url: { type: String, require: false },
+    repository: { type: String, require: false },
+
+    published: { type: Boolean, required: true, default: false },
+    body: {
+      code: { type: String, required: true },
+    },
+  },
+  { timestamps: true },
+);

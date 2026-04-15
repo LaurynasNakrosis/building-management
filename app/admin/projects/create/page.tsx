ca8264fd16f93a8c9f6e856f09f12d1834f226bd
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2026-04-15 16:12:04 +0100

    Add link to create new project: Introduce a navigation link on the AdminProjectsPage for users to easily access the project creation page, enhancing user experience.

--- app/admin/projects/create/page.tsx
+++ app/admin/projects/create/page.tsx
@@ -1,18 +1,16 @@
-'use client';
-
 import { AdminNav } from '@/app/components/AdminNav';
 import Link from 'next/link';
 
-export default function AdminProjectsPage() {
+export default function CreateProjectPage() {
   return (
-    <div>
+    <div className=''>
       <AdminNav />
       <div className='pl-6 pt-24 md:pl-24'>
         <Link
-          href='/admin/projects/create'
+          href='/admin/projects'
           className='flex items-center justify-center text-zinc-400 hover:text-white text-sm mb-6 w-full max-w-[12rem]'
         >
-          Create Project
+          ← Back to Projects
         </Link>
       </div>
     </div>
